193542aa3748c726222f45cc630fd9b69eb01e58
Comst <750145240@qq.com>
2016-11-05 12:16:24 +0800

    initial project

---
+++ LZPhotoEditor/LZPhotoEditor/Classes/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LZPhotoEditor
+//
+//  Created by comst on 16/11/5.
+//  Copyright © 2016年 com.comst1314. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LZPhotoEditor/LZPhotoEditor/Classes/Controllers/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LZPhotoEditor
+//
+//  Created by comst on 16/11/5.
+//  Copyright © 2016年 com.comst1314. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
